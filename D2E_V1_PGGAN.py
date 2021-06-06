c358bab95dc9398ccda3b02ff6d241f51572e56a
disanda <skyanda1@qq.com>
2021-06-06 09:22:58 +0800

    V2 for PGGAN horse256

--- D2E_V1_PGGAN.py
+++ D2E_V1_PGGAN.py
@@ -82,7 +82,8 @@ def train(generator = None, tensor_writer = None):
             result_all = generator(w1)
             imgs1 = result_all['image']
         #w2 = E(imgs1.cuda(),height=6,alpha=1) # height:8 -> 1024, 7->512, 6->256
-        w2 =  E(imgs1)
+        #w2 =  E(imgs1)
+        w2, _ = E(imgs1)
         #w2 = w2.squeeze().squeeze()
         imgs2=generator(w2)['image']
 
